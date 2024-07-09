22d8e490f12fff1479901a8f81cf6a875d1e8a07
Eagle <lzyphil@gmail.com>
2024-07-09 15:04:08 +0200

    add react table

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import DashboardStatsGrid from "./DashboardStatsGrid";
 import TransactionChart from "./TransactionChart";
 import BuyerProfilePieChart from "./BuyerProfilePieChart";
+import RecentOrders from "./RecentOrders";
+import PopularProducts from "./PopularProducts";
 export default function Dashboard() {
     return (
     <div className="flex flex-col gap-4">
@@ -9,6 +11,10 @@ export default function Dashboard() {
         <div className="flex flex-row gap-4 w-full">
             <TransactionChart/>
             <BuyerProfilePieChart/>
+        </div>
+        <div className="flex flex-row gap-4 w-full">
+            <RecentOrders/>
+            <PopularProducts/>
         </div>
     </div>)
 }
